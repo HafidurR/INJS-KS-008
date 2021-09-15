6ca9ccc8515f99190bf146f87b21303b4fc431a3
HafidurR <deifah126@gmail.com>
2021-09-16 00:00:03 +0700

    tambah sesi 5

---
+++ sesi-05/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.use(express.static("public"))
+
+
+app.get('/pertama', (req, res) => {
+    res.send('./public')
+})
+
+app.get('/pertama', (req, res) => {
+    res.sendFile('./public/index.html', {root : __dirname})
+})
+
+app.get('/kedua', (req, res) => {
+    res.sendFile('./public/trial.html', {root : __dirname})
+})
+
+app.get('/ketiga', (req, res) => {
+    res.sendFile('./public/trial2.html', {root : __dirname})
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port : ${port}`);
+})
