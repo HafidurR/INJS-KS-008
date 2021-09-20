dd6ebd3b0af75c298f40f4339b8fd6056093b137
HafidurR <deifah126@gmail.com>
2021-09-20 20:20:06 +0700

    edit coding di sesi-06

--- sesi-06/app.js
+++ sesi-06/app.js
@@ -2,21 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 const data = require('./data.json')
+const fs = require('fs')
 
+app.use(express.urlencoded( {extended: true} )) //agar express mau menerima inputan 
 app.use(express.static('public'))//supaya express bisa melihat folder public yang kita buat
 app.set('view engine', 'ejs')
 
-app.get('/home', (req, res) => {
-    res.render('index.ejs', {
-        nama: 'M. Hafidurrohman',
-        semester: 7,
-        universitas: 'Universitas Nurul Jadid',
-        alamat: 'Probolinggo'
-    })//merender index.ejs yg sdah dibuat sbelumnya
+app.get('/', (req, res) => {
+  
+  if(req.query.nama === undefined) {
+      res.render('coba.ejs', { data: data })
+  } else {
+      let input = req.query.nama
+      let cari = data.find((e) => {
+        return (input.toLowerCase() === e.nama.toLowerCase())
+      })
+      res.render('coba.ejs', { data: [cari] })
+  }//merender index.ejs yg sdah dibuat sbelumnya
 })
 
-app.get('/', (req, res) => {
-    res.send(req.query)
+app.get('/:id', (req, res) => {
+    let find = data.find(item => {
+      return item.id_pengguna === Number(req.params.id)
+    })
+    res.send(find) 
+})
+
+app.post('/create', (req, res) => {
+    //console.log(req.body)
+    let ex = {
+      id_pengguna: data.length,
+      nama: req.body.nama,
+      email: req.body.email,
+      alamat: req.body.alamat,
+    }
+    data.push(ex)
+    fs.writeFileSync('./data.json', JSON.stringify(data, null, 2), 'utf-8')
+    res.redirect('/') // GET
 })
 
 app.listen(port, () => {
