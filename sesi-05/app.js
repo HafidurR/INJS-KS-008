3707b13e5e48dd037f52ccde25abcd3a7ceadd81
HafidurR <deifah126@gmail.com>
2021-09-26 20:21:13 +0700

    edit dikit

--- sesi-05/app.js
+++ sesi-05/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 
