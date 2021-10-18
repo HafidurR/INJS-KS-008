4e44e2a0b3b648c655e412cd1f2e85bf5e86fbc8
HafidurR <deifah126@gmail.com>
2021-10-18 22:44:16 +0700

    edit dikit sesi-11

--- sesi-11/db.js
+++ sesi-11/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg')
 const db = new Pool({
-  user: 'postgres',
-  password: 'postgresql',
-  host: 'localhost',
-  database: 'db_barang',
-  port: 5432,
+  user: 'postgres', //username pada postgres
+  password: 'postgresql', //password pada postgres
+  host: 'localhost', // bisa digati ke 127.0.0.1
+  database: 'db_barang', //nama database yg akan kita pakai
+  port: 5432, // port yg kita pakai pda postgres 
 })
 
 module.exports = db;
